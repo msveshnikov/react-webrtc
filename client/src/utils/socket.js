6a24fdb6e47757d7ed58e4169524b5660c236376
Igor Agapov <aio350@jetstyle.ru>
2022-01-27 12:34:28 +0500

    updated

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SERVER_URI = 'http://localhost:4000'
+const SERVER_URI = import.meta.DEV ? 'http://localhost:4000' : ''
 
 const socket = io(SERVER_URI)
 
