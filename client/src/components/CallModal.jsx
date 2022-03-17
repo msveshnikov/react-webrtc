ecd6e61c9e47558baf47d8d1c8ab5aef10e879c7
Max Sveshnikov <msveshnikov@gmail.com>
2022-03-17 21:47:33 +0100

    Format

--- client/src/components/CallModal.jsx
+++ client/src/components/CallModal.jsx
@@ -1,28 +1,28 @@
-import { BsCameraVideo, BsPhone } from 'react-icons/bs'
-import { FiPhoneOff } from 'react-icons/fi'
+import { BsCameraVideo, BsPhone } from "react-icons/bs";
+import { FiPhoneOff } from "react-icons/fi";
 
 export const CallModal = ({ callFrom, startCall, rejectCall }) => {
-  const acceptWithVideo = (video) => {
-    const config = { audio: true, video }
-    startCall(false, callFrom, config)
-  }
+    const acceptWithVideo = (video) => {
+        const config = { audio: true, video };
+        startCall(false, callFrom, config);
+    };
 
-  return (
-    <div className='call-modal'>
-      <div className='inner'>
-        <p>{`${callFrom} is calling`}</p>
-        <div className='control'>
-          <button onClick={() => acceptWithVideo(true)}>
-            <BsCameraVideo />
-          </button>
-          <button onClick={() => acceptWithVideo(false)}>
-            <BsPhone />
-          </button>
-          <button onClick={rejectCall} className='reject'>
-            <FiPhoneOff />
-          </button>
+    return (
+        <div className="call-modal">
+            <div className="inner">
+                <p>{`${callFrom} is calling`}</p>
+                <div className="control">
+                    <button onClick={() => acceptWithVideo(true)}>
+                        <BsCameraVideo />
+                    </button>
+                    <button onClick={() => acceptWithVideo(false)}>
+                        <BsPhone />
+                    </button>
+                    <button onClick={rejectCall} className="reject">
+                        <FiPhoneOff />
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
--- server/utils/initSocket.js
+++ server/utils/initSocket.js
@@ -1,35 +1,35 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from "nanoid";
 
-const users = {}
+const users = {};
 
 export default function initSocket(socket) {
-  let id
+    let id;
 
-  const emit = (userId, event, data) => {
-    const receiver = users[userId]
-    if (receiver) {
-      receiver.emit(event, data)
-    }
-  }
+    const emit = (userId, event, data) => {
+        const receiver = users[userId];
+        if (receiver) {
+            receiver.emit(event, data);
+        }
+    };
 
-  socket
-    .on('init', () => {
-      id = nanoid(5)
-      users[id] = socket
-      console.log(id, 'connected')
-      socket.emit('init', { id })
-    })
-    .on('request', (data) => {
-      emit(data.to, 'request', { from: id })
-    })
-    .on('call', (data) => {
-      emit(data.to, 'call', { ...data, from: id })
-    })
-    .on('end', (data) => {
-      emit(data.to, 'end')
-    })
-    .on('disconnect', () => {
-      delete users[id]
-      console.log(id, 'disconnected')
-    })
+    socket
+        .on("init", () => {
+            id = nanoid(5);
+            users[id] = socket;
+            console.log(id, "connected");
+            socket.emit("init", { id });
+        })
+        .on("request", (data) => {
+            emit(data.to, "request", { from: id });
+        })
+        .on("call", (data) => {
+            emit(data.to, "call", { ...data, from: id });
+        })
+        .on("end", (data) => {
+            emit(data.to, "end");
+        })
+        .on("disconnect", () => {
+            delete users[id];
+            console.log(id, "disconnected");
+        });
 }
